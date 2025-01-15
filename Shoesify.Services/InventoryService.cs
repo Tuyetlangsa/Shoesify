7b03d512ab96b72bfbccdf9bb244550012dc9f91
Hoàng Long Dương <longheo242@gmail.com>
2025-01-15 11:45:57 +0700

    add validator for create inventory request

--- Shoesify.Services/InventoryService.cs
+++ Shoesify.Services/InventoryService.cs
@@ -3,6 +3,7 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
+using FluentValidation;
 using Shoesify.Entities.Models;
 using Shoesify.Services.Abstractions;
 using Shoesify.Services.Requests;
@@ -13,15 +14,25 @@ namespace Shoesify.Services
     {
         private readonly ShoesifyContext _context;
         private readonly JwtTokenService _jwtTokenService;
+        private readonly IValidator<CreateInventoryRequest> _validator;
 
-        public InventoryService(ShoesifyContext context, JwtTokenService jwtTokenService)
+        public InventoryService(ShoesifyContext context, JwtTokenService jwtTokenService, IValidator<CreateInventoryRequest> validator)
         {
             _context = context;
             _jwtTokenService = jwtTokenService;
+            _validator = validator;
         }
         
         public async Task<int> CreateAnInventory(CreateInventoryRequest request)
         {
+            var validationResult = await _validator.ValidateAsync(request);
+
+            if (!validationResult.IsValid)
+            {
+                var errorMessages = string.Join("; ", validationResult.Errors.Select(e => e.ErrorMessage));
+                throw new ValidationException($"Validation failed: {errorMessages}");
+            }
+            
             (string userId, string role) = _jwtTokenService.GetIdAndRoleFromToken();
             Inventory inv = new Inventory()
             {
